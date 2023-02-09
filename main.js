349d3fb18cf8ddeaf89e7f0ab60b2a2adaf8ad8f
Mekhalfa yassine <120168407+yassinemekhalfa@users.noreply.github.com>
2023-02-09 18:20:18 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,70 @@
+// change navbar styles on scroll
+
+window.addEventListener('scroll', () =>{
+    document.querySelector('nav').classList.toggle
+    ('Window-scroll', window.scrollY > 0)
+})
+
+
+// show/hide nav menu
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+
+menuBtn.addEventListener('click' , () => {
+    menu.style.display = "flex";
+    closeBtn.style.display ="inline-block";
+    menuBtn.style.display = "none";
+})
+
+// close nav menu
+const closenav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closenav)
+
+
+$(document).ready(function(){
+    $(".content").slice(0, 4).show();
+    $("#loadMore").on("click", function(e){
+      e.preventDefault();
+      $(".content:hidden").slice(0, 4).slideDown();
+      if($(".content:hidden").length == 0) {
+        $("#loadMore").text("No Content").addClass("noContent");
+      }
+    });
+    
+  })
+
+
+
+
+  const items = document.querySelectorAll('.item');
+
+const expand = (item, i) => {
+  items.forEach((it, ind) => {
+    if (i === ind) return;
+    it.clicked = false;
+  });
+  gsap.to(items, {
+    width: item.clicked ? '15vw' : '8vw',
+    duration: 2,
+    ease: 'elastic(1, .6)' });
+
+
+  item.clicked = !item.clicked;
+  gsap.to(item, {
+    width: item.clicked ? '42vw' : '15vw',
+    duration: 2.5,
+    ease: 'elastic(1, .3)' });
+
+};
+
+items.forEach((item, i) => {
+  item.clicked = false;
+  item.addEventListener('click', () => expand(item, i));
+});
