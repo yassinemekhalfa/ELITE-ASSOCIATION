94b74a8fcc7733c469f84acf01459d880b058e30
yassine <mekhalfayacine@gmail.com>
2023-02-28 14:11:17 +0100

    weewwwd

--- script.js
+++ script.js
@@ -1,3 +1,67 @@
+// change navbar styles on scroll
+
+window.addEventListener('scroll', () =>{
+    document.querySelector('nav').classList.toggle
+    ('Window-scroll', window.scrollY > 0)
+})
+
+
+// show/hids faq answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+  faq.addEventListener('click' , () => {
+    faq.classList.toggle('open');
+
+
+    // change icon 
+    const icon = faq.querySelector('.faq__icon i');
+    if(icon.className === 'uil uil-plus') {
+      icon.className = "uil uil-minus";
+    } else {
+      icon.className = "uil uil-plus";
+
+    }
+  })
+})
+
+
+// show/hide nav menu
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+
+menuBtn.addEventListener('click' , () => {
+    menu.style.display = "flex";
+    closeBtn.style.display ="inline-block";
+    menuBtn.style.display = "none";
+})
+
+// close nav menu
+const closenav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closenav)
+
+
+$(document).ready(function(){
+    $(".content").slice(0, 4).show();
+    $("#loadMore").on("click", function(e){
+      e.preventDefault();
+      $(".content:hidden").slice(0, 4).slideDown();
+      if($(".content:hidden").length == 0) {
+        $("#loadMore").text("No Content").addClass("noContent");
+      }
+    });
+    
+  })
+
+
 //getting all required
 const start_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
